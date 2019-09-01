4d6e6c468eda162a6946f22bca944d0466ea1a0c
Woo Jia Hao <woojiahao@gmail.com>
2019-09-01 12:42:59 +0800

    Run script now displays running containers

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ if [[ -z $BOT_TOKEN ]]; then
 	echo "Bot token must be added as an environment variable to your machine to execute this bot"
 else
 	docker run -e BOT_TOKEN=${BOT_TOKEN} -d discord-docker:latest
+	docker container ls
 fi
